4bc512804f2c5e2294e25a254e1c477d396dcb39
GabrielSilper <gabrielsilper.main@gmail.com>
2023-12-12 06:39:26 -0400

    config: adicionei o typeorm e o driver do postgres no projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const DB_DATABASE = process.env.DB_DATABASE as string;
+const DB_HOST = process.env.DB_HOST as string;
+const DB_USER = process.env.DB_USER as string;
+const DB_PASSWORD = process.env.DB_PASSWORD as string;
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: DB_HOST,
+      database: DB_DATABASE,
+      username: DB_USER,
+      password: DB_PASSWORD,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
